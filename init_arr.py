e9711ad2a1e9d000d537195f710f8a6fef7d6fbf
divya-manirajan <divya.manirajan@gmail.com>
2025-12-30 00:56:07 -0600

    added parts

---
+++ init_arr.py
@@ -0,0 +1,12 @@
+rows = 6
+cols = 5
+
+arr = [["[   ]" for col in range(cols)] for row in range(rows)]
+
+# for row in range(rows):
+#     for col in range(cols):
+#         arr[row][col] = "[   ]"
+
+
+for row in arr:
+    print(*row)
